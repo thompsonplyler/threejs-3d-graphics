789358886c145e9113af8da37e66a45fc168e651
thompsonplyler <nealok.g@gmail.com>
2019-11-17 13:06:34 -0500

    Adjusted model appearance for Twitch.

--- Exercise Files/chapter-01/01_02/begin/main.js
+++ Exercise Files/chapter-01/01_02/begin/main.js
@@ -1,33 +1,47 @@
 function init() {
-    const gui = new dat.GUI()
+    // const gui = new dat.GUI()
     const scene = new THREE.Scene();
     const boxGrid = getBoxGrid(10, 1.5) // defines dimensions of box for the scene
     // box.position.y = 0.5; // this elevates the box above the plane, but it only works when the box is 1 height.
     // box.position.y = box.geometry.parameters.height / 2; // this works regardless of the height of the box. 
     const sphere = getSphere(.05)
     const plane = getPlane(80)
+    const loader = new THREE.OBJLoader(); // used to ingest a model later.
+    const textureLoader = new THREE.TextureLoader(); // used to apply textures to a model
+    const map = textureLoader.load('./model/rab-obj/textures/rabadon_diffuse.tga.png') // defines texture in variable for later
+    const emissive = textureLoader.load('./model/rab-obj/textures/rabadon_emissive.tga.png') // defines texture in variable for later
 
-    const loader = new THREE.OBJLoader();
-    const textureLoader = new THREE.TextureLoader();
-    const map = textureLoader.load('./model/rab-obj/textures/rabadon_diffuse.tga.png')
-    console.log("Logging variable: map", map)
-    const material = new THREE.MeshPhongMaterial({ map: map });
-    console.log(loader)
+    // defines material to be used in loaded object. Property names
+    // correspond to names variables, but variables could be x, y, or squirrel.
+    const material = new THREE.MeshPhongMaterial({ map: map, emissive: emissive });
+    material.name = "material"
 
 
+    // loader calls OBJ file. Errors with OBJLoader2 and GLTFLoader, but
+    // principle is the same. 
+    loader.load('model/rab-obj/source/rabadonhatfinished.obj', object => {
 
-    loader.load('model/rab-obj/source/rabadonhatfinished.obj', function (object) {
-
-        console.log(object)
+        // looks for .isMesh property in the array of nodes in the object
+        // if true, assigns it the material defined above.
         object.traverse(function (node) {
             if (node.isMesh) node.material = material
         })
+
+        // scaling/rotation tools for presentation | different for any project
+        let scaleVal = 0.1
+        object.scale.x = scaleVal
+        object.scale.y = scaleVal
+        object.scale.z = scaleVal
+        object.rotation.y = 10
+
         scene.add(object)
     });
 
+    // currently irrelevant as plane is not being called, but this defines flat plane
     plane.rotation.x = 90 // rotate the plane 90 radians around its x-axis
     plane.rotation.x = Math.PI / 2 // rotate the plane 90 DEGREES around its x-axis
     plane.name = 'plane-1';
+    //
 
     enableFog = false
     if (enableFog) { scene.fog = new THREE.FogExp2(0xffffff, 0.02) }
@@ -38,14 +52,17 @@ function init() {
     directionalLight.position.y = 10
     directionalLight.position.z = 10
 
+
     const ambientLight = getAmbientLight(1)
     scene.add(ambientLight)
 
-    scene.add(plane); // adds the plane to the scene
-    scene.add(boxGrid); // adds the box as a child of the plane
+    // scene.add(plane); // adds the plane to the scene
+    // scene.add(boxGrid); // adds the box as a child of the plane
     scene.add(directionalLight)
 
-    const helper = new THREE.CameraHelper(directionalLight.shadow.camera)
+    // helper shows where the light is aiming, but shouldn't be used in production
+    // const helper = new THREE.CameraHelper(directionalLight.shadow.camera)
+
     const camera = new THREE.PerspectiveCamera(
         45, //fov
         window.innerWidth / window.innerHeight, //aspect
@@ -53,28 +70,75 @@ function init() {
         1000 // far clipping plane
     ); // defines camera for the scene
 
-    scene.add(helper)
+    // scene.add(helper)
 
     directionalLight.position.y = 2
-    gui.add(directionalLight, 'intensity', 0, 10)
-    gui.add(directionalLight.position, 'y', 0, 20)
-    gui.add(directionalLight.position, 'x', 0, 20)
-    gui.add(directionalLight.position, 'z', 0, 20)
+
+    // gui.add(directionalLight, 'intensity', 0, 10)
+    // gui.add(directionalLight.position, 'y', 0, 20)
+    // gui.add(directionalLight.position, 'x', 0, 20)
+    // gui.add(directionalLight.position, 'z', 0, 20)
     // gui.add(directionalLight, 'penumbra', 0, 1)
-
-    // by default camera and object are created at 0,0,0
-    // view intersects with box created. 
-    // adjusting camera's x,y,z and lookat() value consts the 
-    // viewer observe the object
-
-    camera.position.x = 2;
-    camera.position.y = 3;
-    camera.position.z = 8;
-    camera.lookAt(new THREE.Vector3(0, 0, 0)); // Vector3 is a coordinate in 3D space.
+    // gui.add(material, "opacity", 0, 1)
 
 
+    // below sets up camera rig for precision control
+    // incidentally, breaks OrbitControls.
 
-    const renderer = new THREE.WebGLRenderer(); // use WebGL instead of SVG or canvas
+    const cameraZPosition = new THREE.Group();
+    const cameraYPosition = new THREE.Group();
+    const cameraYRotation = new THREE.Group();
+    const cameraXRotation = new THREE.Group();
+    const cameraZRotation = new THREE.Group();
+    cameraXRotation.name = "cameraXRotation";
+    cameraYRotation.name = "cameraYRotation";
+    cameraYPosition.name = "cameraZPosition";
+    cameraZPosition.name = "cameraZPosition";
+    cameraZRotation.name = "cameraZRotation";
+
+    cameraZPosition.position.z = 100
+    cameraZPosition.position.y = 1
+
+    cameraZRotation.add(camera)
+    cameraYPosition.add(cameraZRotation)
+    cameraZPosition.add(cameraYPosition)
+    cameraXRotation.add(cameraZPosition)
+    cameraYRotation.add(cameraXRotation)
+    scene.add(cameraYRotation)
+
+    new TWEEN.Tween({ val: 100 })
+        .to({ val: 15 }, 500)
+        .easing(TWEEN.Easing.Quadratic.InOut)
+        .onUpdate(function () {
+            cameraZPosition.position.z = this.val
+        })
+        .start()
+
+    new TWEEN.Tween({ val: 0 })
+        .to({ val: 1 }, 400)
+        .onUpdate(function () {
+            material.opacity = this.val
+        })
+        .start()
+
+    new TWEEN.Tween({ val: 1.3 })
+        .to({ val: -.05 }, 1000)
+        .easing(TWEEN.Easing.Quadratic.InOut)
+        .onUpdate(function () {
+            cameraYRotation.rotation.y = this.val
+        })
+        .start()
+
+    // gui.add(cameraZPosition.position, "z", 0, 100)
+    // gui.add(cameraYRotation.rotation, "y", -Math.PI, Math.PI)
+    // gui.add(cameraXRotation.rotation, "x", -Math.PI, Math.PI)
+
+    // end precision control 
+
+
+
+
+    const renderer = new THREE.WebGLRenderer({ alpha: true }); // use WebGL instead of SVG or canvas
 
     renderer.setSize(window.innerWidth, window.innerHeight); // determine width/height of renderer
     // renderer.setClearColor('#ffffff')
@@ -124,6 +188,9 @@ function update(renderer, scene, camera, controls) {     // update seems to be a
         scene,
         camera
     ); // render, obviously.
+
+    let cameraZPosition = scene.getObjectByName("cameraZPosition")
+    TWEEN.update();
 
     requestAnimationFrame(function () {
         update(renderer, scene, camera, controls);
